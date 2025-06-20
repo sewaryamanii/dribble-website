c05b5a3a21bed465d3a8809a4d72baf6855ebe3a
sewaryamanii <sewar.alyamani@gmail.com>
2025-06-21 02:10:25 +0300

    last update

--- src/test/java/DribbleWebsite/DribbleWebsite/AppTest.java
+++ src/test/java/DribbleWebsite/DribbleWebsite/AppTest.java
@@ -17,6 +17,7 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.BeforeTest;
@@ -96,7 +97,7 @@ public class AppTest extends TestData {
 
 	@Test(priority = 2, enabled = false)
 // we have to scroll so that we can click on filters button since the test can't reach it if we don't 
-	public void Filtersinlogoutmode() throws InterruptedException, SQLException, IOException {
+	public void Filters() throws InterruptedException, SQLException, IOException {
 
 		js.executeScript("window.scrollTo(0,400)");
 		Thread.sleep(3000);
@@ -124,12 +125,6 @@ public class AppTest extends TestData {
 		File SrcFile = scrShot.getScreenshotAs(OutputType.FILE);
 		File DestFile = new File("src/test/ScreenShot/" + mydate + ".jpg");
 		FileUtils.copyFile(SrcFile, DestFile);
-
-	}
-
-	@Test(priority = 3, enabled = false)
-	public void TimeframeFilter() throws InterruptedException {
-
 		js.executeScript("window.scrollTo(400,500)");
 		Thread.sleep(3000);
 
@@ -140,9 +135,12 @@ public class AppTest extends TestData {
 		WebElement targetOption = driver
 				.findElement(By.cssSelector(".btn-dropdown-options.sets-querystring li[data-value='" + value + "'] a"));
 		targetOption.click();
+
 	}
 
-	@Test(priority = 4, enabled = false)
+
+
+	@Test(priority = 3, enabled = false)
 	public void TypeInSearch() throws InterruptedException {
 		WebElement searchbar = driver.findElement(By.id("autocomplete-1-input"));
 		Thread.sleep(3000);
@@ -155,7 +153,7 @@ public class AppTest extends TestData {
 
 	}
 
-	@Test(priority = 5, enabled = false)
+	@Test(priority = 4, enabled = false)
 	public void TypeInSearchGibberish() throws InterruptedException, IOException {
 		WebElement searchbar2 = driver.findElement(By.id("autocomplete-1-input"));
 		searchbar2.click();
@@ -177,64 +175,66 @@ public class AppTest extends TestData {
 		File SFile = scrShott.getScreenshotAs(OutputType.FILE);
 		File DFile = new File("src/test/ScreenShot/" + mydate + ".jpg");
 		FileUtils.copyFile(SFile, DFile);
-	}
-
-	@Test(priority = 6, enabled = false)
-	public void FiltersBar() throws InterruptedException {
 		js.executeScript("window.scrollTo(0,400)");
-		Thread.sleep(2000);
-		WebElement arrow = driver.findElement(By.linkText(">"));
-		arrow.click();
 
+		Thread.sleep(2000);
+		WebElement arrow = driver.findElement(By.xpath("//span[@class='scroll scroll-forward']//a[@href='#']"));
+		arrow.click();
 	}
 
-	@Test(priority = 7, enabled = true)
+
+
+	@Test(priority = 5, enabled = false)
 	public void PopularButton() throws InterruptedException {
 		js.executeScript("window.scrollTo(0,400)");
-		Thread.sleep(3000);
 
-		WebElement dropdown = driver.findElement(
-			    By.cssSelector(".form-btn.outlined.btn-dropdown-link.btn-dropdown-link--small")
-			);
+		Thread.sleep(3000);
+		WebElement dropdown = driver
+				.findElement(By.cssSelector(".form-btn.outlined.btn-dropdown-link.btn-dropdown-link--small"));
 		dropdown.click();
 		String value = popularbutton[randomselect];
-		WebElement targetOption = driver.findElement(
-			    By.cssSelector(".btn-dropdown-options a[data-track-view='" + value + "']")
-			);
-
+		WebElement targetOption = driver
+				.findElement(By.cssSelector(".btn-dropdown-options a[data-track-view='" + value + "']"));
 		targetOption.click();
 
-		
-		
-		
+	}
+
+
+
+
+	@Test(priority = 6, enabled = false)
+	public void LoginN() throws InterruptedException {
+		driver.findElement(By.cssSelector(".site-nav__login.btn2.btn2--medium")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.cssSelector("input[placeholder='Enter email or username']"))
+				.sendKeys("sewar.alyamani@gmail.com");
+		Thread.sleep(1000);
+		driver.findElement(By.xpath(
+				"//form[@action='/session/send_email_otp']//button[@type='submit'][normalize-space()='Continue']"))
+				.click();
+
+	}
+
+	@Test(priority = 7, enabled = false)
+	public void SaveElementWithoutLogIn() throws InterruptedException, IOException {
+		Thread.sleep(90000);
+		driver.findElement(By.cssSelector(".shot-thumbnail-link.dribbble-link.js-shot-link")).click();
+		Thread.sleep(1500);
+		driver.findElement(By.cssSelector(".bucket-shot.btn2.btn2--circle.btn2--tertiary")).click();
+		Thread.sleep(1000);
+
+		TakesScreenshot scrShot = ((TakesScreenshot) driver);
+		File SrcFile = scrShot.getScreenshotAs(OutputType.FILE);
+
+		File DestFile = new File("src/test/ScreenShot/" + mydate + ".jpg");
+		FileUtils.copyFile(SrcFile, DestFile);
 	}
 
 	@Test(priority = 8, enabled = false)
-	public void LoginP() throws SQLException, InterruptedException, IOException {
-
-		driver.findElement(By.cssSelector(".site-nav__login.btn2.btn2--medium")).click();
-		String query = "SELECT * FROM Users WHERE ID = '2';";
-		stmt = con.createStatement();
-		rs = stmt.executeQuery(query);
-		while (rs.next()) {
-			UserName = rs.getString("Username");
-			System.out.println(UserName);
-		}
-		rs = stmt.executeQuery(query);
-
-		while (rs.next()) {
-			Password = rs.getString("Password");
-			System.out.println(Password);
-		}
-
-		WebElement Username = driver.findElement(By.id("login"));
-		Username.sendKeys(UserName);
-
-		WebElement password = driver.findElement(By.id("password"));
-		password.sendKeys(Password);
-		driver.findElement(By.cssSelector(".btn2.btn2--large.btn2--full-width.margin-t-20")).click();
-
-		System.out.println(mydate);
+	public void socialmedealiks() throws InterruptedException, IOException {
+		js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//a[@href='http://www.pinterest.com/dribbble/']//*[name()='svg']")).click();
 
 		Thread.sleep(1000);
 
@@ -243,53 +243,43 @@ public class AppTest extends TestData {
 
 		File DestFile = new File("src/test/ScreenShot/" + mydate + ".jpg");
 		FileUtils.copyFile(SrcFile, DestFile);
-
 	}
 
 	@Test(priority = 9, enabled = false)
-	public void LoginN() throws SQLException {
-		driver.findElement(By.cssSelector(".site-nav__login.btn2.btn2--medium")).click();
-		String query = "SELECT * FROM Users WHERE ID = '" + randomuser + "';";
-		stmt = con.createStatement();
-		rs = stmt.executeQuery(query);
-		while (rs.next()) {
-			UserName = rs.getString("Username");
-			System.out.println(UserName);
-		}
-		rs = stmt.executeQuery(query);
+	public void TapOnRandomBlog() throws InterruptedException {
 
-		while (rs.next()) {
-			Password = rs.getString("Password");
-			System.out.println(Password);
-		}
-		WebElement Username = driver.findElement(By.id("login"));
-		Username.sendKeys(UserName);
-		driver.findElement(By.cssSelector(".btn2.btn2--large.btn2--full-width.margin-t-20")).click();
-
-	}
-
-	@Test(priority = 10, enabled = false)
-	public void Filtersinloginmode() throws InterruptedException, IOException {
-		WebElement searchbar = driver.findElement(By.id("autocomplete-1-input"));
-		Thread.sleep(3000);
-		searchbar.sendKeys("Test Cases");
-		searchbar.sendKeys(Keys.chord(Keys.ENTER));
-		Thread.sleep(3000);
-
-		WebElement searchbar2 = driver.findElement(By.id("autocomplete-0-input"));
-		driver.findElement(By.cssSelector("aa-ClearIcon")).click();
-		searchbar2.sendKeys("betdgae");
-		searchbar2.sendKeys(Keys.chord(Keys.ENTER));
-
-		System.out.println(mydate);
-
+		driver.findElement(By.cssSelector("button[aria-label='Toggle mobile menu']")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//a[@class='site-nav-main__link'][normalize-space()='Blog']")).click();
 		Thread.sleep(1000);
 
-		TakesScreenshot scrShot = ((TakesScreenshot) driver);
-		File SrcFile = scrShot.getScreenshotAs(OutputType.FILE);
+		js.executeScript("window.scrollTo(0,800)");
 
-		File DestFile = new File("src/test/ScreenShot/" + mydate + ".jpg");
-		FileUtils.copyFile(SrcFile, DestFile);
+		driver.findElement(By.cssSelector(".post.group")).click();
 
 	}
+
+	@Test(priority = 10, enabled = true)
+	public void Support () throws InterruptedException, IOException {
+		js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
+
+	 driver.findElement(By.xpath("//a[normalize-space()='Support']")).click();
+	WebElement supportsearch = driver.findElement(By.id("search-input"));
+				
+	supportsearch.sendKeys("How to Create a New Service");
+	supportsearch.sendKeys(Keys.chord(Keys.ENTER));
+		
+	driver.findElement(By.cssSelector(".article__preview.intercom-force-break")).click();
+	Thread.sleep(1000);
+	js.executeScript("window.scrollTo(0,500)");
+	TakesScreenshot scrShot = ((TakesScreenshot) driver);
+	File SrcFile = scrShot.getScreenshotAs(OutputType.FILE);
+
+	File DestFile = new File("src/test/ScreenShot/" + mydate + ".jpg");
+	FileUtils.copyFile(SrcFile, DestFile);
+	
+	}
+	
+	
+	
 }
